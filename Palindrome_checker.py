68bca393bfdecee76d0542e1be816e020c2ab620
wadva <wadva474@gmail.com>
2022-01-20 23:54:12 +0100

    palindrome check

---
+++ Palindrome_checker.py
@@ -0,0 +1,10 @@
+#Build Palindrome Checker
+string= input('Enter string:')
+reverse_string= string[ : :-1]
+
+if string== reverse_string:
+    print('string is palindrome')
+else:
+    print('string is not palindrome')
+
+
